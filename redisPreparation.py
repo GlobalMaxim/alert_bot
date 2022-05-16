ac13a2648959a3ca0c1389ffdae09385d9493018
Maxim <verchenko1998@gmail.com>
2022-05-16 03:40:10 +0300

    #Updated redis get_regions method

--- redisPreparation.py
+++ redisPreparation.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import redis
 import json
 
@@ -5,7 +6,7 @@ class Redis_Preparation():
     # def __init__(self, result):
     #     self.result = result
     
-    def get_data_from_redis(self, data):
+    def get_regions_from_redis(self, data):
         try:
             redis_client = redis.Redis()
             reg_from_redis = redis_client.get('reg')
@@ -23,4 +24,35 @@ class Redis_Preparation():
             return result
         except Exception as ex:
             with open('log/redis-log.txt', 'a') as file:
-                file.write(ex)
+                file.write(str(ex))
+    
+    def set_temp_data_to_redis(self, message):
+        try:
+            redis_client = redis.Redis(db=1)
+            user_id = message.from_user.id
+            username = message.from_user.username
+            first_name = message.from_user.first_name
+            last_name = message.from_user.last_name
+            language_code = message.from_user.language_code
+
+            user_from_redis = redis_client.get(str(user_id))
+
+            
+            if user_from_redis is None:
+                user_data = {'user_id':user_id, 'first_name': first_name, 'last_name': last_name, 'username': username, 'language_code': language_code, 'count_exec_scripts': 1, 'modified_at': str(datetime.now())}
+                redis_client.set(str(user_id), json.dumps(user_data))
+            else:
+                user_data_from_redis = json.loads(user_from_redis)
+                user_data_from_redis['count_exec_scripts'] += 1
+                user_data_from_redis['modified_at'] = str(datetime.now())
+                redis_client.set(str(user_id), json.dumps(user_data_from_redis))
+        except Exception as ex:
+            with open('log/redis-log.txt', 'a') as file:
+                file.write(str(ex))
+    
+    def get_users_data_from_redis():
+        try:
+            redis_client = redis.Redis(db=1)
+        except Exception as ex:
+            with open('log/redis-log.txt', 'a') as file:
+                file.write(str(ex))
