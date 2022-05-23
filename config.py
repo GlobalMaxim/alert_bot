0c80b0b106e81f92cd2b0df6bc6d3be80723eb28
Maxim <verchenko1998@gmail.com>
2022-05-23 16:28:57 +0300

    config py ignoring

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = '5268436236:AAEGDtar1F0naCXTbDVDukQIDYVL8xhGZFA'
-admin_id = '389837052'
-API_KEY = "4083272368ae9c7a6912cf489ec087de8b162cfd"
-DATABASE_PASS = "Vfrcbv19981408"
-DATABASE_HOST = 'alerttelegrambot.cfhtqnpm0xnk.us-east-2.rds.amazonaws.com'
-OS = 'Windows'
-API_KEY = "4083272368ae9c7a6912cf489ec087de8b162cfd"
