84e3fd8e3bfbdc206acfdb8e5ca40983fb996d65
Maxim <verchenko1998@gmail.com>
2022-06-10 14:44:38 +0300

    updated crone mail

--- crone/crone.py
+++ crone/crone.py
@@ -18,20 +18,16 @@ async def update_api_data():
     api_data = api_parse_info()
     r = Redis_Preparation()
     res = r.get_and_update_regions_from_redis(api_data)
+    mail = Mailing()
+    await mail.send_mailing(bot)
     if res['is_updated'] == True:
-        print('waiting 10 sec')
-        await asyncio.sleep(10)
         await parse_photo()
         print('waiting for 20 sec sleep')
         await asyncio.sleep(20)
+        print('start parsing 2')
         await parse_photo()
-        
         print('updated in 20 sec')
-    try:
-        mail = Mailing()
-        await mail.send_mailing(bot)
-    except Exception as ex:
-        print(ex)
+    
 
 async def send_message_to_admin(bot):
     await bot.send_message(admin_id, f'Сохранено {values[0]} новых пользователей и обновлено {values[1]} старых пользователя')
